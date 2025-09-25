a7cdf1e9bf58d555a8d9b2dfe95a5e39e8ff9999
mauroibz <mauro@surus.dev>
2025-09-25 19:21:00 +0000

    [ADD] translation task v1 included

--- download_models.sh
+++ download_models.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 models=(
-    "01-ai/Yi-1.5-9B-Chat" # 4096
-    "bigscience/bloomz-7b1" # 2048
-    "01-ai/Yi-1.5-6B-Chat" # 4096
-    "HuggingFaceH4/zephyr-7b-beta" # 8192
-    "NousResearch/Hermes-3-Llama-3.1-8B" # 8192
-    "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
-    "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
-    "CohereLabs/c4ai-command-r7b-12-2024"
+    "tencent/Hunyuan-MT-7B" #works
+    "ByteDance-Seed/Seed-X-PPO-7B"
+    "ByteDance-Seed/Seed-X-Instruct-7B"
 )
 
 for model in "${models[@]}"; do
